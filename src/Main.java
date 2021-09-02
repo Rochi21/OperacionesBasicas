d2b2e4d8ba4eac7e2963e5714e8779eb2ba290a7
pilar <pilar@192.168.0.3>
2021-09-01 22:28:15 -0500

    se creo una función que suma dos numeros

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+class Main {
+
+
+	public static void main(String[] args) {
+		float suma = sumar(1, 2);
+		System.out.println(suma);
+	}
+	public static float sumar (float num1, float num2){
+		return num1+num2;
+}
+}
