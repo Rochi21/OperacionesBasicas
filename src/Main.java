928be7d3cf6a46614b1809026770aff5830a2662
pilar <pilar@192.168.0.3>
2021-09-01 22:39:02 -0500

    se añadio un tercer número a la función suma

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,10 @@ class Main {
 
 
 	public static void main(String[] args) {
-		float suma = sumar(1, 2);
+		float suma = sumar(1,2,3);
 		System.out.println(suma);
 	}
-	public static float sumar (float num1, float num2){
-		return num1+num2;
+	public static float sumar (float num1, float num2, float num3){
+		return num1+num2+num3;
 }
 }
