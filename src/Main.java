9eded17248f1d046221a528bbb3b4b416e6d8e79
pilar <pilar@192.168.0.3>
2021-09-01 22:47:54 -0500

    se creo la función restar de dos numeros

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,15 @@ class Main {
 	public static void main(String[] args) {
 		float suma = sumar(1,2,3);
 		System.out.println(suma);
+		
+		float resta = restar(1,2);
+		System.out.println(resta);
 	}
 	public static float sumar (float num1, float num2, float num3){
 		return num1+num2+num3;
+}
+
+public static float restar (float num1, float num2){
+	return num1-num2;
 }
 }
